04226066f5882b1a23a0d8393516d03846e670e5
tjbanuelos <tomasbanuelos@Tomass-MacBook-Pro-6.local>
2025-04-15 14:18:54 -0500

    update to logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -10,6 +10,9 @@ let map = L.map('map', {
   zoom: 4
 });
 
+// Create layer for labels
+let labelLayer = L.layerGroup();
+
 // Then add the 'basemap' tile layer to the map.
 basemap.addTo(map)
 
@@ -22,22 +25,14 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   
   // This function determines the color of the marker based on the depth of the earthquake.
   function getColor(depth) {
-      if (depth < 10) return "#00ff00";
-      else if (depth < 30) return "#00fff0";
-      else if (depth < 50) return "#fff000";
-      else if (depth < 70) return "#fff0f0";
-      else if (depth < 90) return "#ff000f";
-      else return "#ff0000";
-  }
+    if (depth <= 10)  return "#FEB24C";
+    else if (depth <= 30 ) return "#FD8D3C";  
+    else if (depth <= 50) return "#FC4E2A"; 
+    else if (depth <= 70) return "#E31A1C";  
+    else if (depth <= 90) return "#BD0026";
+    else return "#800026";
+}
 
-  // to calculate the color and radius.
-  function styleInfo(feature) {
-      return {
-          fillColor: getColor(feature.geometry.coordinates[2]),
-          fillOpacity: 0.8,
-          wieght: 0.5,
-      };
-  }
 
 // This function determines the radius of the earthquake marker based on its magnitude.
 function getRadius(magnitude) {
@@ -50,35 +45,57 @@ function getRadius(magnitude) {
   pointToLayer: function (feature, latlng) {
       return L.circleMarker(latlng,{ 
     // Set the style for each circleMarker using our styleInfo function.
-      style: styleInfo(feature),
-      radius: getRadius(feature.properties.mag)
+      radius: getRadius(feature.properties.mag),
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
     // Create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
       }).bindPopup(`<h1>${feature.properties.place}</h1> <hr> <h3> Magnitude: ${feature.properties.mag}</h3> <hr> <h3> Depth: ${feature.geometry.coordinates[2]}</h3>`)
   }
-}).addTo(map)
+}).addTo(map);
 
+// Lable layer
+// const label = L.marker(latlng,{
+  // icon: L.divIcon({
+    // className: 'test',
+    // html: `<strong>${tariff}%</strong>`,
+    // iconSize: [30, 12],
+    // iconAnchor: [15, -12]
+  // })
+// });
 
+  // label.addTo(labelLayer);
+
+// Legend
 let info = L.control({
   position: "bottomright"
 });
 
 // Then add all the details for the legend
-info.onAdd = function () {
+info.onAdd = function (map) {
   let div = L.DomUtil.create("div", "info legend");
 
   // Initialize depth intervals and colors for the legend
-  var depths = ['-10-10', '10-30', '30-50', '50-70', '70-90', '90+'];
-  var colors = ['green', 'blue', 'yellow', 'pink', 'orange', 'red'];
+  let depths = [-10, 10, 30, 50, 70, 90];
+  let labels = [];
 
   // Loop through our depth intervals to generate a label with a colored square for each interval.
   for (let i= 0; i < depths.length; i++){
-    color = colors[i],
-    label = depths[i],
-    opacity = 1
-  };
+    let from = depths[i];
+    let to  = depths[i + 1];
+
+    labels.push(
+      `<i style="background:${getColor(from + 1)}"></i> ${from}${to ? '&ndash;' + to : '+'}`
+    );
+  }
+
+  div.innerHTML = labels.join('<br>');
   return div;
+
 };
 
 // Finally, add the legend to the map.
-info.addTo(map)
+info.addTo(map);
 });
